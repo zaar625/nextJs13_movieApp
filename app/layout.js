c14460038e43c06bdefee02ecde6f9f1e7863f16
이상윤 <zaar625@gmail.com>
2023-09-04 14:58:17 +0900

    create the navbar

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
 import Provider from './Provider'
+import NavBar from '@/components/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
           {/* Header */}
           <Header/>
           {/* Nav */}
+          <NavBar/>
           {/* SearchBox */}
             {children}
         </Provider>
